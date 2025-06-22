76363495946c488b25f6ce80fed509b6c84abdd9
gorkemoa <gorkemoa35@gmail.com>
2025-06-23 01:27:54 +0300

    video_player bağımlılığı eklendi ve ilgili platform bağımlılıkları güncellendi. Ayrıca, pubspec.yaml dosyasında video dosyası varlığı kontrolü için gerekli değişiklikler yapıldı. pubspec.lock dosyasında Dart ve Flutter SDK sürümleri güncellendi.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import google_sign_in_ios
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -21,4 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
